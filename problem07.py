56e281f2e7461621186f0bbff35e89157e4cdda8
Allie Carver <allie_carver23@hotmail.com>
2014-08-07 18:34:37 -0300

    Added docstrings

--- problem07.py
+++ problem07.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python2
-import math
-def check_prime(n):
-	factors=[]
-	for i in xrange(2, int(math.sqrt(n))+1):
-			if float(n)/i%1==0:
-			
-				factors.extend([i])
-	if len(factors) is 0:
-		return True
-	else:
-		return False
-def question7():
-	x=2
-	y=0
-	while y < 10001:
-		if check_prime(x):
-			y+=1
-		if x==2:
-			x+=1
-		elif x >= 3:
-			x+=2
-	print x-2
-question7()		
+#!-*-coding=utf8 -*-
+from math import sqrt as sqrt
+import time
+
+
+"""
+========================
+Project Euler Problem 07
+========================
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+"""
+#Fastest way to creat primes	
+def ero_sieve(n):
+    primes=set(i for i in xrange(3,n+1,2))
+    for i in set(primes):
+        j=2
+        while i*j<n:
+            primes.discard(i*j)
+            j+=1 
+    primes.add(2)
+    return primes
+
+def problem07():
+    primes=list(ero_sieve(200000))
+    primes.sort()
+    return primes[10000]
+	
+	
+if __name__=='__main__':
+    time1=time.time()
+    print problem07()
+    time2=time.time()
+    print time2-time1
--- problem09.py
+++ problem09.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python2
+#!-*-coding=utf8 -*-
+
 import time
-time1=time.time()
+
+"""
+========================
+Project Euler Problem 09
+========================
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a**2 + b**2 = c**2
+
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+"""
+
 def pythagorean():
     for a in xrange(500):
         for b in xrange(a+1,500):
@@ -8,6 +25,9 @@ def pythagorean():
                 if a**2+b**2==c**2:
                     return a*b*c
                     
-print pythagorean()
-time2=time.time()
-print time2-time1
+if __name__=='__main__':
+    time1=time.time()
+    print pythagorean()
+    time2=time.time()
+    print time2-time1	
+
